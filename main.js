49ff0c0789641e44705312ff5ba58e99921c45bb
Jared schroeder <schroedertheory@gmail.com>
2020-09-08 01:36:21 -0600

    finished landing section and project section

---
+++ main.js
@@ -0,0 +1,8 @@
+// function rotateImage() {
+//       var img = document.getElementById('gear');
+//
+//       var i;
+//       for (i = 0; i < 80; i++) {
+//           img.style.transform = 'rotate(1deg)';
+//       }
+//    }
